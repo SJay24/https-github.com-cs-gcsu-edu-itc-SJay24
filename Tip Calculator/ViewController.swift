185267f9d1a5a44cd00fae2f4c83b9d3218e7da4
Sam Johnson <samuel.johnson3@bobcats.gcsu.edu>
2021-02-08 21:01:23 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by samuel johnson3 on 2/8/21.
+//  Copyright © 2021 Sam Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
